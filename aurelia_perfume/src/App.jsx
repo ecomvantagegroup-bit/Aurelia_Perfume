41ed1a45c28dfb1e9708029464a2d6ddb4c2691c
StellArc24 <stellararcgames@gmail.com>
2026-07-30 00:29:11 +0530

    vue project creation

---
+++ aurelia_perfume/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>You did it!</h1>
+
+      <p>
+        Visit{" "}
+        <a
+          href="https://vuejs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          vuejs.org
+        </a>{" "}
+        to read the documentation
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ aurelia_perfume/src/router/index.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  // Example routes
+  // {
+  //   path: "/",
+  //   element: <Home />,
+  // },
+]);
+
+export default router;
